1c4001069e183922b7faebbb326681db3c1b48cd
Romain GONÇALVES <romain.goncalves@gmail.com>
2017-06-25 00:02:11 +0200

    Basic TOC Component

---
+++ src/components/Decateam/TableOfContents.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class TableOfContents extends React.Component {
+
+    constructor(){
+        super();
+    }
+
+    render() {
+        return (
+            <div className="toc">
+
+            </div>
+        );
+    }
+}
+
+export default TableOfContents
