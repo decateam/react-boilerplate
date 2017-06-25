d8796dcca2a25912c1c370c0fb91550f57b2a380
Pierre Cruz <keilo@live.fr>
2017-06-25 12:22:42 +0200

    Load HTML from textarea to JSON and render it after that

--- src/components/Decateam/HtmlParser.js
+++ src/components/Decateam/HtmlParser.js
@@ -1,22 +1,47 @@
 import React from 'react';
 
+var himalaya = require('himalaya');
+var toHTML = require('himalaya/translate').toHTML
 
 class HtmlParser extends React.Component {
 
+    // Override constructor
     constructor() {
         super();
-        this.state = { htmldocument: "<div>Example HTML string</div>" };
-        this.fetchExternalHTML = this.fetchExternalHTML.bind(this);
+
+        // Binding events
+        this.handleTextareaKeyUp = this.keyUpHandler.bind(this, 'htmlInput');
+
+        // Define initial state
+        this.state = {
+            jsonifyHtmlDocument: {},
+            htmlifyJsonNodes: ''
+        };
     }
 
-    fetchExternalHTML() {
-      var data = require(this.props.filename);
-      this.setState({htmldocument: data});
+    // Load HTML from textarea
+    keyUpHandler(refName, e) {
+
+        let htmlParsed = himalaya.parse(e.target.value);
+        let htmlifyJson = toHTML(htmlParsed);
+
+        this.setState({
+            jsonifyHtmlDocument: htmlParsed,
+            htmlifyJsonNodes: htmlifyJson
+        });
     }
 
+    // Render component
     render() {
       return (
-        <div>Hello</div>
+          <div>
+            <div className="input">
+                <textarea id="source" onKeyUp={this.handleTextareaKeyUp} ref="htmlInput" placeholder="HTML a charger"></textarea>
+            </div>
+            <div className="output">
+                <div className="content" dangerouslySetInnerHTML={{__html: this.state.htmlifyJsonNodes}}></div>
+            </div>
+          </div>
         )
     }
 }
